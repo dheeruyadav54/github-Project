25b61fef28bb788a196cc2574927b9a9aff1aa30
Dheeraj Yadav <dheeruyadav54@gmail.com>
2024-09-23 00:47:05 +0200

    Revert "added line 3 wrong line"
    This reverts commit c3a3ce41664c182235c0b723856f2b1ee75e217c.

--- AC.py
+++ AC.py
@@ -1,3 +1,2 @@
 print ("hello world")
-print ("added this correct line which needs to be reverted")
-print ("wrong line which needs to be reverted")
+print ("added this wrong line which needs to be reverted")
